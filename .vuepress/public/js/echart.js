0fd793995988180a31982a7597774a41880677ed
yaoyuan <yaoyuan@nsfocus.com>
2024-01-05 15:31:46 +0800

    feat: v1-删除v1版本下文件

--- .vuepress/public/js/echart.js
+++
@@ -1,6 +0,0 @@
-// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
-import * as echarts from "echarts/core";
-import { CanvasRenderer } from "echarts/renderers";
-import { GlobeComponent } from "echarts-gl/components";
-
-echarts.use([GlobeComponent, CanvasRenderer]);
--- docs/extend/photo.md
+++
@@ -1,8 +0,0 @@
----
-title: 相册
-# subSidebar: false
-date: 2022-9-28 14:47:01
----
-
-
-## Test
